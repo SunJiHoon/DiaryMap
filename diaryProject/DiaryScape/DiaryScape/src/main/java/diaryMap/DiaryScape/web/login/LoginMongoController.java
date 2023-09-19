df4800ac2bf7835f9540ecedf2a9b9e97d6fabbd
Sun-JiHoon <cangogi1@naver.com>
2023-09-19 16:27:31 +0900

    mongoDB 컨트롤러 realId  쿠키를 뱉어내도록 수정

--- diaryProject/DiaryScape/DiaryScape/src/main/java/diaryMap/DiaryScape/web/login/LoginMongoController.java
+++ diaryProject/DiaryScape/DiaryScape/src/main/java/diaryMap/DiaryScape/web/login/LoginMongoController.java
@@ -46,7 +46,7 @@ public class LoginMongoController {
         //로그인 성공 처리 TODO
         //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
         Cookie idCookie = new Cookie("memberId",
-                String.valueOf(loginMember.getId()));
+                String.valueOf(loginMember.getRealid()));
         response.addCookie(idCookie);
 
         MemberWithoutId mem = new MemberWithoutId(loginMember.getLoginId(), loginMember.getName());
