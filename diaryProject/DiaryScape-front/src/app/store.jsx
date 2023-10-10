c8f55278bda1814a1459924629d03fecbcee0f2d
harutea <snowhanami@gmail.com>
2023-10-10 20:56:54 +0900

    create new redux slice 'startnodeSlice' and use it in my_tripmap

--- diaryProject/DiaryScape-front/src/app/store.jsx
+++ diaryProject/DiaryScape-front/src/app/store.jsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
 import userSlice from '../reducer/user_slice'
 import tripSlice from '../reducer/trip_slice'
+import startnodeSlice from "../reducer/startnode_slice"
 import { combineReducers } from "@reduxjs/toolkit"
 
 const reducer = combineReducers({
     user: userSlice,
     trip: tripSlice,
+    startnode: startnodeSlice,
 })
 
 export const store = configureStore({
