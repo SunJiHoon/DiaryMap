81744f25e91a57d009feacbe5954cc87de0ad844
harutea <snowhanami@gmail.com>
2023-10-31 17:09:25 +0900

    apply redux persist on login

--- diaryProject/DiaryScape-front/src/main.jsx
+++ diaryProject/DiaryScape-front/src/main.jsx
@@ -4,13 +4,18 @@ import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+export const persistor = persistStore(store)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <Provider store={store}>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </PersistGate>
     </Provider>
   // </React.StrictMode>
 )
