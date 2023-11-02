98050af1d35fe44d275a337ccf9282a7164ad077
harutea <snowhanami@gmail.com>
2023-11-03 03:07:12 +0900

    update styles of starting node searching section in my_tripmap

--- diaryProject/DiaryScape-front/src/my_tripmap/my_tripmap.jsx
+++ diaryProject/DiaryScape-front/src/my_tripmap/my_tripmap.jsx
@@ -5,7 +5,7 @@ import { selectStartnode, clearStartnode } from "../reducer/startnode_slice"
 import {
     Box,
     Input,
-    Button, 
+    Button,
     Heading,
     Modal,
     ModalOverlay,
@@ -30,7 +30,7 @@ const MyTripmap = () => {
 
 
     const dispatch = useDispatch()
-    
+
     // const testLoginData = {
     //     name:"Tester",
     //     loginId: "tester_id"
@@ -40,7 +40,7 @@ const MyTripmap = () => {
 
     const username = useSelector((state) => state.user.name)
 
-    
+
 
     const [reviewData, setReviewData] = useState([])
     const [newReviewValue, setNewReviewValue] = useState([])
@@ -63,21 +63,21 @@ const MyTripmap = () => {
     let source
     const onSearch = ((e) => {
 
-        if(source) {
+        if (source) {
             source.cancel()
         }
 
         source = axios.CancelToken.source()
-        
+
         // setSearchValue(e.target.value)
         setStartNodeSelected(false)
         // console.log(e.target.value)
-        
+
         const searchValueReplaced = searchValue.replace(/ /g, "%20")
         console.log("search: " + searchValueReplaced)
 
         // console.log("axios get 요청 : " + "http://localhost:8080/api/openApi/start/list?userKeyword=" + searchValueReplaced)
-        
+
         setSearchResultDataLoading(true)
         client.get("/api/openApi/start/list?userKeyword=" + searchValue, { cancelToken: source.token })
             .then((res) => {
@@ -131,11 +131,11 @@ const MyTripmap = () => {
 
     const onNewReviewSubmit = useCallback((e) => {
         e.preventDefault()
-        if(newReviewValue=="") {
+        if (newReviewValue == "") {
             console.log("새 리뷰 제목 비어있음!")
             return
         }
-        if(!startNodeSelected) {
+        if (!startNodeSelected) {
             console.log("시작 장소 선택되지 않음!")
             return
         }
@@ -144,18 +144,18 @@ const MyTripmap = () => {
         const telReplaced = selectedData.tel.replace(/ /g, "%20")
         const titleReplaced = selectedData.title.replace(/ /g, "%20")
         console.log(newReviewDateValue)
-        client.post("/api/obj/create?mapName="+newReviewNameReplaced
-            +"&date="+newReviewDateValue
-            +"&addr1="+addr1Replaced
-            +"&relativeX="+selectedData.relativeX
-            +"&relativeY="+selectedData.relativeY
-            +"&contentid="+selectedData.contentid
-            +"&contentTypeId="+selectedData.contentTypeId
-            +"&tel="+telReplaced
-            +"&title="+titleReplaced
-            +"&mapx="+selectedData.mapx
-            +"&mapy="+selectedData.mapy
-            , {}, {withCredentials:true})
+        client.post("/api/obj/create?mapName=" + newReviewNameReplaced
+            + "&date=" + newReviewDateValue
+            + "&addr1=" + addr1Replaced
+            + "&relativeX=" + selectedData.relativeX
+            + "&relativeY=" + selectedData.relativeY
+            + "&contentid=" + selectedData.contentid
+            + "&contentTypeId=" + selectedData.contentTypeId
+            + "&tel=" + telReplaced
+            + "&title=" + titleReplaced
+            + "&mapx=" + selectedData.mapx
+            + "&mapy=" + selectedData.mapy
+            , {}, { withCredentials: true })
             .then((res) => {
                 client.get('/api/obj/list').then((res) => {
                     setReviewData(res.data)
@@ -164,10 +164,11 @@ const MyTripmap = () => {
 
         setNewReviewValue('')
         setStartNodeSelected(false)
+        onClose()
     })
 
     useEffect(() => {
-        if(isTest) {
+        if (isTest) {
             setReviewData([
                 {
                     title: "부산 리뷰",
@@ -192,99 +193,101 @@ const MyTripmap = () => {
     }, [])
 
     return (
-    <Box p={6}>
-        <Heading as="h2" size="xl" mb={6}>
-            <Box display="inline" color="blue">{username}</Box>의 여행 리스트
-        </Heading>
-        <Button colorScheme="teal" variant="outline" mb={6} onClick={() => dispatch(clearUser())}>로그아웃</Button>
-        <Box>
+        <Box p={6}>
+            <Heading as="h2" size="xl" mb={6}>
+                <Box display="inline" color="blue">{username}</Box>의 여행 리스트
+            </Heading>
+            <Button colorScheme="teal" variant="outline" mb={6} onClick={() => dispatch(clearUser())}>로그아웃</Button>
+            <Box>
 
-            <Button w="60%" maxW={500} colorScheme="teal" onClick={onOpen} mb={10}>새 여행 작성</Button>
+                <Button w="60%" maxW={500} colorScheme="teal" onClick={onOpen} mb={10}>새 여행 작성</Button>
 
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>새 여행 추가</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <form onSubmit={onNewReviewSubmit} onKeyDown={(e) => {
-                            if(e.code == "Enter") {
-                                e.preventDefault()
-                            }
-                        }}>
-                            <Box display="flex" justifyContent="center">
-                                <Box display="flex" flexDirection="column" justifyContent="center" w="100%" maxW="500px" mt={2} mb={6}>
-                                <Input type="text" placeholder="새 여행 이름" value={newReviewValue} onChange={onNewReviewChange} mb={2} />
-                                <Input type="date" value={newReviewDateValue} onChange={onNewReviewDateChange} mb={2} />
-                                <Box display="flex" mb={2}>
-                                    <Input
-                                        type="text"
-                                        placeholder="시작 장소"
-                                        value={searchValue}
-                                        onChange={(e) => {setSearchValue(e.target.value)}}
-                                        mr={2}
-                                    />
-                                    <Button onClick={onSearch} colorScheme="teal" variant="outline">
-                                        검색
-                                    </Button>
+                <Modal isOpen={isOpen} onClose={onClose}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>새 여행 추가</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <form onSubmit={onNewReviewSubmit} onKeyDown={(e) => {
+                                if (e.code == "Enter") {
+                                    e.preventDefault()
+                                }
+                            }}>
+                                <Box display="flex" justifyContent="center">
+                                    <Box display="flex" flexDirection="column" justifyContent="center" w="100%" maxW="500px" mt={2} mb={6}>
+                                        <Input type="text" placeholder="새 여행 이름" value={newReviewValue} onChange={onNewReviewChange} mb={2} />
+                                        <Input type="date" value={newReviewDateValue} onChange={onNewReviewDateChange} mb={2} />
+                                        <Box display="flex" mb={2}>
+                                            <Input
+                                                type="text"
+                                                placeholder="시작 장소"
+                                                value={searchValue}
+                                                onChange={(e) => { setSearchValue(e.target.value) }}
+                                                mr={2}
+                                            />
+                                            <Button onClick={onSearch} colorScheme="teal" variant="outline">
+                                                검색
+                                            </Button>
+                                        </Box>
+                                        <Button type="submit" colorScheme="teal">여행 생성</Button>
+                                    </Box>
                                 </Box>
-                                <Button type="submit" colorScheme="teal">여행 생성</Button>
-                                </Box>
-                            </Box>
-                        </form>
-                        
-                        <Box display="flex" justifyContent="center" mb={2}>
-                            <Box w="100%" maxW="500px" display="flex" flexDirection="column">
-                                { !startNodeSelected && <>
-                                <Box fontSize="1.4em" mb={4}>시작 가능한 장소</Box>
-                                {searchValue.length == 0 && <Box>장소 이름을 입력해주세요!</Box>}
-                                {searchResultDataLoading && <Box>데이터 불러오는 중...</Box>}
-                                <Box maxH={260} overflowY="scroll">
-                                    {searchResultData.map((result) => (
-                                        <Button colorScheme="teal" variant="outline" h="40px" key={result.contentid} mb={2} onClick={(e) => onStartNodeSelect(result)}>
-                                            {result.title},&nbsp;
-                                            {result.addr1},&nbsp;
-                                            x: {result.mapx}, y: {result.mapy}
-                                        </Button>
-                                    ))}
-                                </Box>
-                                </>}
-                                
-                                { startNodeSelected && <>
-                                <Box fontSize="1.4em" mb={4}>시작 장소 선택됨</Box>
-                                <Button colorScheme="teal" h="40px" mb={2}>
-                                    {selectedData.title},&nbsp;
-                                    {selectedData.addr1}
-                                    {/* ,&nbsp; x: {selectedData.mapx}, y: {selectedData.mapy} */}
-                                </Button>
-                                </>}
-                            </Box>
-                        </Box>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button onClick={onClose} colorScheme="teal">닫기</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
+                            </form>
 
-            <Box display="flex" justifyContent="center" mb={10}>
-                <Box w="100%" maxW="500px" display="flex" flexDirection="column">
-                    <Box fontSize="1.8em" mb={4}>여행 리스트</Box>
-                    {reviewData.map((review) => (
-                        <Button h="70px" key={review.mapId} mb={6} onClick={(e) => onReviewClicked(review)} colorScheme="teal" variant="outline">
-                            <Box display="flex" flexDirection="column">
-                            <Box fontSize="1.6em" mb={1}>{review.reviewtitle}</Box>
-                            <Box>
-                            mapId: {review.mapId} / 시작 좌표: ({review.mapX}, {review.mapY})
+                            <Box display="flex" justifyContent="center" mb={2}>
+                                <Box w="100%" maxW="500px" display="flex" flexDirection="column">
+                                    <Box fontSize="1.4em" mb={2}>시작 장소 선택</Box>
+                                    {/* {startNodeSelected && <Box>{selectedData.contentid}</Box>} */}
+                                    {searchValue.length == 0 && <Box>장소 이름을 입력해주세요!</Box>}
+                                    {searchResultDataLoading && <Box>데이터 불러오는 중...</Box>}
+                                    <Box maxH={260} overflowY="scroll">
+                                        {searchResultData.map((result) => (
+                                            <Button
+                                                border="0px"
+                                                borderBottom="1px"
+                                                borderColor="gray.300"
+                                                borderRadius="0px"
+                                                bgColor={startNodeSelected && selectedData.contentid == result.contentid ? "blue.600" : "white"}
+                                                color={startNodeSelected && selectedData.contentid == result.contentid ? "white" : "black"}
+                                                _hover={{}}
+                                                h="40px"
+                                                key={result.contentid}
+                                                onClick={(e) => onStartNodeSelect(result)}
+                                            >
+                                                {/* {result.contentid} */}
+                                                {result.title},&nbsp;
+                                                {result.addr1},&nbsp;
+                                                x: {result.mapx}, y: {result.mapy}
+                                            </Button>
+                                        ))}
+                                    </Box>
+                                </Box>
                             </Box>
-                            </Box>
-                        </Button>
-                    ))}
-                    {reviewData.length == 0 && <p>새 여행을 작성해주세요!</p>}
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button onClick={onClose} colorScheme="teal">닫기</Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+
+                <Box display="flex" justifyContent="center" mb={10}>
+                    <Box w="100%" maxW="500px" display="flex" flexDirection="column">
+                        <Box fontSize="1.8em" mb={4}>여행 리스트</Box>
+                        {reviewData.map((review) => (
+                            <Button h="70px" key={review.mapId} mb={6} onClick={(e) => onReviewClicked(review)} colorScheme="teal" variant="outline">
+                                <Box display="flex" flexDirection="column">
+                                    <Box fontSize="1.6em" mb={1}>{review.reviewtitle}</Box>
+                                    <Box>
+                                        mapId: {review.mapId} / 시작 좌표: ({review.mapX}, {review.mapY})
+                                    </Box>
+                                </Box>
+                            </Button>
+                        ))}
+                        {reviewData.length == 0 && <p>새 여행을 작성해주세요!</p>}
+                    </Box>
                 </Box>
             </Box>
         </Box>
-    </Box>
     )
 }
 
