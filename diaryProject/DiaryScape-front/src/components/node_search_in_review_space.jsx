3d1c2a712a7707c742f7f9316249cc4aed18918d
harutea <snowhanami@gmail.com>
2023-11-29 19:32:11 +0900

    divide ReviewSpace into multiple components

---
+++ diaryProject/DiaryScape-front/src/components/node_search_in_review_space.jsx
@@ -0,0 +1,122 @@
+import {
+    Box,
+    Button,
+    IconButton,
+    Checkbox,
+    Select,
+    Textarea,
+    Input,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+} from '@chakra-ui/react';
+import {
+    IoChevronDown,
+    IoChevronForward,
+    IoSearch,
+    IoAdd,
+    IoRemove,
+    IoPencil,
+    IoBook,
+    IoChevronBack,
+    IoHome,
+} from 'react-icons/io5';
+
+const NodeSearchInReviewSpace = ({
+    children,
+    isReadonly,
+    searchValue,
+    setSearchValue,
+    onNodeSearch,
+    searchResultDataLoading,
+    nodeSearchSelected,
+    onNodeSearchSelect,
+    selectedData,
+    onPlusSearchNodeClick,
+    searchResultData
+}) => {
+    return (<>
+        {!isReadonly && (
+        <Box maxW="100%" mt={4}>
+          <Box display="flex" mb={2}>
+            <Input
+              w="100%"
+              type="text"
+              placeholder="노드 이름"
+              value={searchValue}
+              onChange={(e) => {
+                setSearchValue(e.target.value);
+              }}
+              mr={1}
+            />
+            <IconButton icon={<IoSearch />} colorScheme="teal" onClick={onNodeSearch}>
+              노드 검색
+            </IconButton>
+          </Box>
+          <Box w="100%" textAlign="end" mb={2}></Box>
+          <Box display="flex" justifyContent="center" mb={4}>
+            <Box w="100%" maxW="500px" display="flex" flexDirection="column">
+              {/* <Box fontSize="1.4em" mb={2}>노드 선택</Box> */}
+              {/* {nodeSearchSelected && <Box>{selectedData.contentid}</Box>} */}
+              {/* {searchValue.length == 0 && <Box>노드 이름을 입력해주세요!</Box>} */}
+              <Box
+                h={260}
+                border="2px"
+                borderRadius={4}
+                borderColor="gray.400"
+                overflowY="scroll"
+                className="custom-scrollbar"
+              >
+                {searchResultDataLoading && <Box>데이터 불러오는 중...</Box>}
+                {!searchResultDataLoading &&
+                  searchResultData.map((result, idx) => (
+                    <Button
+                      key={result.contentid}
+                      border="0px"
+                      borderBottom="1px"
+                      borderColor="gray.300"
+                      borderRadius="0px"
+                      bgColor={
+                        nodeSearchSelected && selectedData.contentid == result.contentid
+                          ? 'blue.600'
+                          : 'white'
+                      }
+                      color={
+                        nodeSearchSelected && selectedData.contentid == result.contentid
+                          ? 'white'
+                          : 'black'
+                      }
+                      _hover={{}}
+                      h="40px"
+                      onClick={(e) => onNodeSearchSelect(result, idx)}
+                    >
+                      {/* {result.contentid} */}
+                      <Box fontWeight="semibold" mr={2}>
+                        {result.title}
+                      </Box>
+                      <Box fontWeight="medium">{result.addr1}</Box>
+                      {/* x: {result.mapx}, y: {result.mapy} */}
+                    </Button>
+                  ))}
+              </Box>
+            </Box>
+          </Box>
+          <IconButton
+            w="100%"
+            icon={<IoAdd />}
+            colorScheme="teal"
+            onClick={() => {
+              if (nodeSearchSelected) onPlusSearchNodeClick(selectedData);
+            }}
+          />
+        </Box>
+      )}
+      </>)
+}
+
+export default NodeSearchInReviewSpace
--- diaryProject/DiaryScape-front/src/reviewspace/reviewspace.jsx
+++ diaryProject/DiaryScape-front/src/reviewspace/reviewspace.jsx
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import LeftBar from '../components/left_bar'
+import NodeSearchInReviewSpace from '../components/node_search_in_review_space'
 import RightBar from '../components/right_bar'
 import RightBarPageDay from '../components/right_bar_page_day'
 import RightBarPageDiary from '../components/right_bar_page_diary'
@@ -558,81 +559,18 @@ const ReviewSpace = () => {
                 colorScheme="gray"
                 onClick={() => navigate('/')}
               />
-              {!isReadonly && (
-                <Box maxW="100%" mt={4}>
-                  <Box display="flex" mb={2}>
-                    <Input
-                      w="100%"
-                      type="text"
-                      placeholder="노드 이름"
-                      value={searchValue}
-                      onChange={(e) => {
-                        setSearchValue(e.target.value);
-                      }}
-                      mr={1}
-                    />
-                    <IconButton icon={<IoSearch />} colorScheme="teal" onClick={onNodeSearch}>
-                      노드 검색
-                    </IconButton>
-                  </Box>
-                  <Box w="100%" textAlign="end" mb={2}></Box>
-                  <Box display="flex" justifyContent="center" mb={4}>
-                    <Box w="100%" maxW="500px" display="flex" flexDirection="column">
-                      {/* <Box fontSize="1.4em" mb={2}>노드 선택</Box> */}
-                      {/* {nodeSearchSelected && <Box>{selectedData.contentid}</Box>} */}
-                      {/* {searchValue.length == 0 && <Box>노드 이름을 입력해주세요!</Box>} */}
-                      <Box
-                        h={260}
-                        border="2px"
-                        borderRadius={4}
-                        borderColor="gray.400"
-                        overflowY="scroll"
-                        className="custom-scrollbar"
-                      >
-                        {searchResultDataLoading && <Box>데이터 불러오는 중...</Box>}
-                        {!searchResultDataLoading &&
-                          searchResultData.map((result, idx) => (
-                            <Button
-                              key={result.contentid}
-                              border="0px"
-                              borderBottom="1px"
-                              borderColor="gray.300"
-                              borderRadius="0px"
-                              bgColor={
-                                nodeSearchSelected && selectedData.contentid == result.contentid
-                                  ? 'blue.600'
-                                  : 'white'
-                              }
-                              color={
-                                nodeSearchSelected && selectedData.contentid == result.contentid
-                                  ? 'white'
-                                  : 'black'
-                              }
-                              _hover={{}}
-                              h="40px"
-                              onClick={(e) => onNodeSearchSelect(result, idx)}
-                            >
-                              {/* {result.contentid} */}
-                              <Box fontWeight="semibold" mr={2}>
-                                {result.title}
-                              </Box>
-                              <Box fontWeight="medium">{result.addr1}</Box>
-                              {/* x: {result.mapx}, y: {result.mapy} */}
-                            </Button>
-                          ))}
-                      </Box>
-                    </Box>
-                  </Box>
-                  <IconButton
-                    w="100%"
-                    icon={<IoAdd />}
-                    colorScheme="teal"
-                    onClick={() => {
-                      if (nodeSearchSelected) onPlusSearchNodeClick(selectedData);
-                    }}
-                  />
-                </Box>
-              )}
+              <NodeSearchInReviewSpace
+                isReadonly={isReadonly}
+                searchValue={searchValue}
+                setSearchValue={setSearchValue}
+                onNodeSearch={onNodeSearch}
+                searchResultDataLoading={searchResultDataLoading}
+                nodeSearchSelected={nodeSearchSelected}
+                onNodeSearchSelect={onNodeSearchSelect}
+                selectedData={selectedData}
+                onPlusSearchNodeClick={onPlusSearchNodeClick}
+                searchResultData={searchResultData}
+              />
             </Box>
             <IconButton
               h="60px"
@@ -676,29 +614,27 @@ const ReviewSpace = () => {
             className="custom-scrollbar"
           >
             {rightBarPage == 0 && (
-
-            <RightBarPageDay
-              isReadonly={isReadonly}
-              nodeInfoData={nodeInfoData}
-              reviews={reviews}
-              setReviews={setReviews}
-              currentDay={currentDay}
-              setCurrentDay={setCurrentDay}
-              dayCheckedList={dayCheckedList}
-              setDayCheckedList={setDayCheckedList}
-              dayModuleList={dayModuleList}
-              setDayModuleList={setDayModuleList}
-              dayMenuOpenList={dayMenuOpenList}
-              setDayMenuOpenList={setDayMenuOpenList}
-              setOnPlusDay={setOnPlusDay}
-              reviewMenuOpen={reviewMenuOpen}
-              setReviewMenuOpen={setReviewMenuOpen}
-              nextDayMenuId={nextDayMenuId}
-              setNextDayMenuId={setNextDayMenuId}
-              getCurNodeRef={getCurNodeRef}
-              changeDayNodeIndexRef={changeDayNodeIndexRef}
-            />
-
+              <RightBarPageDay
+                isReadonly={isReadonly}
+                nodeInfoData={nodeInfoData}
+                reviews={reviews}
+                setReviews={setReviews}
+                currentDay={currentDay}
+                setCurrentDay={setCurrentDay}
+                dayCheckedList={dayCheckedList}
+                setDayCheckedList={setDayCheckedList}
+                dayModuleList={dayModuleList}
+                setDayModuleList={setDayModuleList}
+                dayMenuOpenList={dayMenuOpenList}
+                setDayMenuOpenList={setDayMenuOpenList}
+                setOnPlusDay={setOnPlusDay}
+                reviewMenuOpen={reviewMenuOpen}
+                setReviewMenuOpen={setReviewMenuOpen}
+                nextDayMenuId={nextDayMenuId}
+                setNextDayMenuId={setNextDayMenuId}
+                getCurNodeRef={getCurNodeRef}
+                changeDayNodeIndexRef={changeDayNodeIndexRef}
+              />
             )}
             {rightBarPage == 1 && (
               <RightBarPageDiary
@@ -708,7 +644,7 @@ const ReviewSpace = () => {
                 generateDiaryRef={generateDiaryRef}
               />
             )}
-            </Box>
+          </Box>
         </RightBar>
         
         <NodeMenu
