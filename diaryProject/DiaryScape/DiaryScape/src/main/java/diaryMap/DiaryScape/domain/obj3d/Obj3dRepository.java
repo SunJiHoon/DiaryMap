099700c7c81cbe3de6d9f88a657783a9f2b207b4
Sun-JiHoon <cangogi1@naver.com>
2023-12-04 15:36:46 +0900

    path기반 추천

--- diaryProject/DiaryScape/DiaryScape/src/main/java/diaryMap/DiaryScape/domain/obj3d/Obj3dRepository.java
+++ diaryProject/DiaryScape/DiaryScape/src/main/java/diaryMap/DiaryScape/domain/obj3d/Obj3dRepository.java
@@ -1,13 +1,14 @@
 package diaryMap.DiaryScape.domain.obj3d;
 
-import diaryMap.DiaryScape.domain.member.Member;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface Obj3dRepository extends MongoRepository<Obj3d, String> {
     void deleteById(String id);
+    Optional<Obj3d> findById(String id);
     List<Obj3d> findAll(); // 모든 객체를 가져오는 메서드
 }
---
+++ diaryProject/DiaryScape/DiaryScape/src/main/java/diaryMap/DiaryScape/web/placeRecommend/PlaceRecommendationController.java
@@ -0,0 +1,129 @@
+package diaryMap.DiaryScape.web.placeRecommend;
+
+import diaryMap.DiaryScape.domain.obj3d.Obj3d;
+import diaryMap.DiaryScape.domain.obj3d.Obj3dRepository;
+import diaryMap.DiaryScape.web.obj3d.NodeDTO_for_update;
+import diaryMap.DiaryScape.web.openApi.NodeDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+import static diaryMap.DiaryScape.web.openApi.NodeController.calRelativeX;
+import static diaryMap.DiaryScape.web.openApi.NodeController.calRelativeY;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@Slf4j
+public class PlaceRecommendationController {
+    private final Obj3dRepository obj3dRepository;
+
+    @GetMapping(value = "/placeRecommend/recommendPlace",produces = "application/json")
+    ArrayList<importedPathModule> recommendPlace(
+            //@RequestBody String contentid
+            @RequestParam Map<String, String> paraMap
+    ){
+
+        String userContentid = paraMap.get("contentid");
+        String MapidForStartingNode = paraMap.get("mapid");
+        String curuseridForremovingNode = paraMap.get("loginId");
+
+        Optional<Obj3d> obj3dOptionalforStart = obj3dRepository.findById(MapidForStartingNode);
+        String startX = "";
+        String startY = "";
+        if (obj3dOptionalforStart.isPresent()){
+            Obj3d obj3dforStart = obj3dOptionalforStart.get();
+            startX =obj3dforStart.getStartNode().getMapx();
+            startY =obj3dforStart.getStartNode().getMapy();
+        }
+
+        List<Obj3d> obj3dList = obj3dRepository.findAll();
+        ArrayList<mapidAndDay> mapidAndDayArrayList = new ArrayList<>();
+        for(int i = 0; i < obj3dList.size(); i++){//모든 맵 중에서
+            Obj3d curObj3d = obj3dList.get(i);
+            NodeDTO_for_update[] nodeDTOForUpdatesArr = curObj3d.getJsonArr();
+            if (curObj3d.getMember().getLoginId().compareTo(curuseridForremovingNode) !=0 && nodeDTOForUpdatesArr != null){//jsonArr가 아직 생성 안된 경우는 제외해야 한다.
+                //nodeDTOForUpdates 배열이 contentid와 일치하는 노드를 포함하고 있을까?
+                for(int j = 0; j < nodeDTOForUpdatesArr.length; j++){
+                    NodeDTO_for_update curNodeDTO_for_update = nodeDTOForUpdatesArr[j];
+                    String curContentid = curNodeDTO_for_update.getContentid();
+
+                    if (curContentid != null && curContentid.compareTo(userContentid) == 0){
+                        //curContentid
+                        String curMapid = curObj3d.getId();
+                        String curDay = curNodeDTO_for_update.getVisitDate();
+                        mapidAndDay tempMAD = new mapidAndDay(curMapid, curDay);
+                        mapidAndDayArrayList.add(tempMAD);
+                    }
+                    //맵의 id와 맵의 날짜 저장
+                }
+            }
+        }
+
+        //현재 mapidAndDayArrayList에는 userContentid를 방문한 적 있는 맵과 날짜를 저장하고 있다.
+        //이제부터 mapid를 찾고 해당 날짜를 모아서 리스트로 만들면 된다.
+        ArrayList<importedPathModule> importedPathModuleArrayList = new ArrayList<>();
+        for (int i=0; i < mapidAndDayArrayList.size(); i++){
+            mapidAndDay tempMAD = mapidAndDayArrayList.get(i);
+            String curMapid = tempMAD.getMapid();
+            String curDay = tempMAD.getVisitDate();
+
+            Optional<Obj3d> obj3d = obj3dRepository.findById(curMapid);
+            if (obj3d.isPresent()) {
+                Obj3d actualObj3d = obj3d.get();
+                //username구하기
+                String curUsername = actualObj3d.getMember().getName();
+                //ArrayList<NodeDTO> nodeDTOArrayList; 구하기
+                NodeDTO_for_update[] finding = actualObj3d.getJsonArr();
+                ArrayList<NodeDTO_for_update> curNodeDTOArrayList = new ArrayList<>();
+                for (int j=0; j < finding.length; j++){
+                    if (finding[j].getVisitDate().compareTo(curDay) == 0){
+                        String relativeX = calRelativeX(startX, finding[j].getMapx());
+                        String relativeY = calRelativeY(startY, finding[j].getMapy()); //calRelativeX를 Y에 재활용.
+                        NodeDTO_for_update nodeDTO_for_update = new NodeDTO_for_update(
+                                finding[j].getContentid(),
+                                finding[j].getContentTypeId(),
+                                finding[j].getTitle(),
+                                finding[j].getTel(),
+                                finding[j].getMapx(),
+                                finding[j].getMapy(),
+                                relativeX,
+                                relativeY,
+                                finding[j].getAddr1(),
+                                finding[j].getVisitDate(),
+                                finding[j].getNodeReview()
+                        );
+                        curNodeDTOArrayList.add(nodeDTO_for_update);
+                    }
+                }
+                importedPathModule curimportedPathModule = new importedPathModule(curUsername, curNodeDTOArrayList);
+                importedPathModuleArrayList.add(curimportedPathModule);
+            }
+            else{
+
+            }
+        }
+
+        //맵의 주인공 이름과, 맵에 담긴 노드들 중, 해당 날짜 노드 들을 추천.
+        return importedPathModuleArrayList;
+    }
+
+
+}
+
+@AllArgsConstructor
+@Data
+class mapidAndDay{
+    String mapid;
+    String visitDate;
+}
+@AllArgsConstructor
+@Data
+class importedPathModule{
+    String username;
+    ArrayList<NodeDTO_for_update> NodeDTO_for_updateArrayList;
+}
